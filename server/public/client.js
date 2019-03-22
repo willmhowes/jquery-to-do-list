667b2a3ccae20596470c399e5b0d8a1f8750d29f
Will Howes <willmhowes@gmail.com>
2019-03-22 17:09:52 -0500

    make page load a table at load

--- server/public/client.js
+++ server/public/client.js
@@ -2,25 +2,49 @@ console.log('javascript loaded');
 
 $(document).ready(onReady);
 
+// function to run at page load
 function onReady() {
    console.log('jQuery loaded');
    renderAllTasks();
+
+   $('.btn-change-status').on('click', changeTaskStatus);
+   $('.btn-delete-row').on('click', deleteTask);
 }
 
+// GET data from /tasks and append to DOM
 function renderAllTasks() {
    $.ajax({
       type: 'GET',
       url: '/tasks',
    }).then(function (response) {
-      $('#task-list').empty();
+      $('#table-body').empty();
+      let status = null;
 
-      for(let task of response) {
-         $('#task-list').append(`
-         <li>${task.task}</li>
+      for (let task of response) {
+         status = isTaskComplete(task.is_complete);
+         console.log(status);
+
+         $('#table-body').append(`
+            <tr>
+               <td>${task.task}</td>
+               <td>${status}</td>
+               <td><button class="btn-change-status">Make Complete</button></td>
+               <td><button class="btn-delete-row">Delete</button></td>
+            </tr>
       `);
       }
-
    }).catch(function (error) {
+      console.log('Failed to retrieve tasks:', error);
+      alert(`Error: could not retrieve tasks`);
+   });
+}
 
-   })
+// Checks if task is complete and returns a string
+function isTaskComplete(taskStatus) {
+   console.log('in isTaskComplete', taskStatus);
+   if (taskStatus === true) {
+      return 'complete';
+   } else if (taskStatus === false) {
+      return 'incomplete';
+   }
 }
