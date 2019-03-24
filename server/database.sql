1ac71c4769ce46f51350277daa0958100e313b2f
Will Howes <willmhowes@gmail.com>
2019-03-23 19:35:46 -0500

    update database.sql

--- server/database.sql
+++ server/database.sql
@@ -3,3 +3,21 @@ CREATE TABLE "toDoTable" (
    task varchar(255) NOT NULL,
    is_complete boolean
 );
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Write a heart-felt letter to my grandma', false);
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Dress up like Robin Hood', true);
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Engage in monetary theft', true);
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Donate stolen funds to Wikipedia', true);
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Buy a half-dozen dinner rolls', false);
+
+INSERT INTO "toDoTable" ("task", "is_complete")
+   VALUES ('Sell carboard cutout of Jon Mulaney', false);
