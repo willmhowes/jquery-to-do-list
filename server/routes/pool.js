89c146c072e0821f4c00a50035094a040ed8d983
Will Howes <willmhowes@gmail.com>
2019-03-25 11:47:16 -0500

    add heroku support

--- server/routes/pool.js
+++ server/routes/pool.js
@@ -1,13 +1,36 @@
 const pg = require('pg');
+const url = require('url');
 
-const Pool = pg.Pool;
-const pool = new Pool({
-   database: 'weekend-to-do-app',
-   host: 'localhost',
-   port: 5432,
-   max: 10,
-   idleTimeoutMillis: 30000,
-});
+let config = {};
+
+if (process.env.DATABASE_URL) {
+   // Heroku gives a url, not a connection object
+   // https://github.com/brianc/node-pg-pool
+   const params = url.parse(process.env.DATABASE_URL);
+   const auth = params.auth.split(':');
+
+   config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true, // heroku requires ssl to be true
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+   };
+} else {
+   config = {
+      host: 'localhost',
+      port: 5432,
+      database: 'weekend-to-do-app',
+      max: 10,
+      idleTimeoutMillis: 30000,
+   };
+}
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
    console.log('PostgreSQL connected!');
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const taskRouter = require('./routes/task.router');
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
